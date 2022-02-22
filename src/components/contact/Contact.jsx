81bcc9770efc64a191aae0022e010f1546b995a0
Aleksei Poluhin <Poluhin1979@outlook.com>
2022-02-22 23:06:57 +0300

    добавил страницу контактов

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 import './contact.scss';
 
 export default function Contact() {
+	const handleSubmit = (e) => {
+		e.preventDefoult();
+	};
+
 	return (
 		<div className="contact" id="contact">
-			contact
+			<div className="left">
+				<img src="./assets" alt="logo" />
+			</div>
+			<div className="right">
+				<h1>Контакты.</h1>
+				<form onSubmit={handleSubmit}>
+					<input type="text" placeholder="Email" />
+					<textarea placeholder="Message" />
+					<button type="submit">Отправить</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,15 @@ import './portfolio.scss';
 export default function Portfolio() {
 	return (
 		<div className="portfolio" id="portfolio">
-			Portfolio
+			<h1>Portfolio</h1>
+			<ul>
+				<li>Сайт 1</li>
+				<li>Сайт 2</li>
+				<li>Сайт 3</li>
+				<li>Сайт 4</li>
+				<li>Сайт 5</li>
+			</ul>
+			<div className="contaner" />
 		</div>
 	);
 }
