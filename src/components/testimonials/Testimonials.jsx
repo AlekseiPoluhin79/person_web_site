413465e1a1cbf0ccf65836e39710c54cadd3507c
Aleksei Poluhin <Poluhin1979@outlook.com>
2022-02-23 22:20:48 +0300

    добавляю отзывы

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,7 +4,28 @@ import './testimonials.scss';
 export default function Testimonials() {
 	return (
 		<div className="testimonials" id="testimonials">
-			Testimonials
+			<h1>Отзывы</h1>
+			<div className="container">
+				<div className="card">
+					<div className="top">
+						<img className="left" src="./assets/right-arrow.png" alt="" />
+						<img
+							className="user"
+							src="https://i11.fotocdn.net/s118/996f882843c28ad9/public_pin_l/2688122229.jpg"
+							alt=""
+						/>
+						<img className="right" src="./assets/youtube.png" alt="" />
+					</div>
+					<div className="center">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero dicta iure consequuntur quidem,
+						nobis rerum.
+					</div>
+					<div className="bottom">
+						<h3>Alex</h3>
+						<h4>CEO of Don</h4>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
